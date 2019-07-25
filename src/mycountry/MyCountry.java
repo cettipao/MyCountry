a8236209ceb7b69e1621a69b55d7b9906a38596c
cettipao <cettipao@live.com.ar>
2019-07-25 15:05:26 -0300

    Login Agregado

--- src/mycountry/MyCountry.java
+++ src/mycountry/MyCountry.java
@@ -16,6 +16,8 @@ public class MyCountry {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login login = new Login();
+        login.setVisible(true);
     }
     
 }
